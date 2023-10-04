6184b1b97e771e0480df9d330e0e05656f24f92f
Misako Moriyama <misa108d23106@yahoo.co.jp>
2023-10-04 09:57:18 +0900

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :additives, only: [:index, :show]
+  resources :allergens, only: [:index, :show]
   # Defines the root path route ("/")
   # root "articles#index"
 end
