73a8e4e7ff7da70e9935fe95bf13cf0b0c19d83c
Efren Franco <efrenrfranco@gmail.com>
2023-10-07 13:58:02 +0900

    added seeds and more search skeleton

--- app/models/additive.rb
+++ app/models/additive.rb
@@ -1,5 +1,15 @@
 class Additive < ApplicationRecord
   has_many :ingredients
+  has_many :allergens
+
   validates :name, :information, :danger_level, presence: true
   validates :name, uniqueness: true
+
+
+  include PgSearch::Model
+  pg_search_scope :search_by_aa,
+                  against: %i[name],
+                  associated_against: { allergens: [:name] },
+                  using: { tsearch: { prefix: true } }
+
 end
