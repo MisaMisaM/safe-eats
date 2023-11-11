f2d7fe59073626a328afc0d4e0fc8546489db136
Misako Moriyama <misa108d23106@yahoo.co.jp>
2023-11-11 17:21:02 -0600

    destroyed landing controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema[7.0].define(version: 2023_11_05_025330) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,12 +70,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_05_025330) do
     t.datetime "updated_at", null: false
     t.index ["additive_id"], name: "index_ingredients_on_additive_id"
     t.index ["allergen_id"], name: "index_ingredients_on_allergen_id"
-  end
-
-  create_table "results", force: :cascade do |t|
-    t.text "text"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "scans", force: :cascade do |t|
